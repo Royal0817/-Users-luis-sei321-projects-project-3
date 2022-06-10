a7aec8218acc2432399eb389f1975958bdb17f83
royal0817 <martinezl27737@gmail.com>
2022-06-10 11:29:01 -0400

    improved map search

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,7 +1,20 @@
 import React, { useState, useCallback, useRef} from 'react'
 import { useLoadScript, GoogleMap, Marker, InfoWindow} from '@react-google-maps/api'
-import Search from './Search'
-
+// import Search from './Search'
+// import Locate from './Search'
+// import {Search, Locate } from './Search' not working? 
+import usePlacesAutoComplete, {
+    getGeocode,
+    getLatLng,
+} from 'use-places-autocomplete';
+import {
+    Combobox,
+    ComboboxInput,
+    ComboboxPopover,
+    ComboboxList,
+    ComboboxOption,
+} from '@reach/combobox';
+import '@reach/combobox/styles.css';
 
 
 const center = { lat: 38.8960499, lng: -77.0648878}
@@ -48,16 +61,23 @@ const Map = () => {
         mapRef.current = map;
     }, []);
 
+    const panTo = useCallback(({ lat, lng }) => {
+        mapRef.current.panTo({ lat, lng });
+        mapRef.current.setZoom(14);
+      }, []);
+    
+
     if (LoadError) return 'Error Loading Maps';
     if (!isLoaded) return  <h1>Loading....</h1>; 
     
 
     return (
         <>
-        <Search />
-
+        <Search panTo={panTo} />
+        <Locate panTo={panTo} />
         {/* // Renders map onto page */}
         <GoogleMap 
+            className='map'
             mapContainerStyle={mapContainerStyle} 
             zoom={13} 
             center={center} 
@@ -68,14 +88,18 @@ const Map = () => {
         {/* Sets marker for reccomendation */}
         {markers.map((marker) => (
             <Marker 
-                key={marker.time.toISOString()} 
-                 position={{ lat: marker.lat, lng: marker.lng}} 
-                 onClick={() => {
-                     setMarkerSelected(marker)
-                 }}/>
+                // key={marker.time.toISOString()} 
+                key={`${marker.lat}-${marker.lng}`}
+                position={{ lat: marker.lat, lng: marker.lng}} 
+                onClick={() => {
+                    setMarkerSelected(marker)
+                }}/>
         ))} 
         {markerSelect ? (
-            <InfoWindow position={{lat: markerSelect.lat, lng: markerSelect.lng}}>
+            <InfoWindow position={{lat: markerSelect.lat, lng: markerSelect.lng}}
+            onCloseClick={() => {
+                setMarkerSelected(null);
+              }}>
                 <div className='Referred'> 
                     <h2>
                         Referred by me
@@ -89,6 +113,80 @@ const Map = () => {
 
         )
     } 
+
+
+function Locate({ panTo }) {
+    return (
+        <button
+            className='locate'
+            onClick={() => {
+                navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    panTo({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                    });
+                },
+                () => null
+                );
+            }}
+        >
+        <img src='https://www.kindpng.com/picc/m/11-115956_icon-google-maps-compass-north-hd-png-download.png' alt='compass' />
+        </button>
+    );
+}
+
+function Search({ panTo }) {
+    const {
+        ready, 
+        value, 
+        suggestions: { status, data },
+        setValue,
+        clearSuggestions, } 
+        = usePlacesAutoComplete({
+        requestOptions: {
+            location: { lat: () => 38.8960499, lng: () => -77.0648878 },
+            radius: 150 * 1000, 
+        },
+    });
+
+    const handleInput = (e) => {
+        setValue(e.target.value);
+        };
+
+    const handleSelect = async (address) => {
+        setValue(address, false);
+        clearSuggestions();
+
+        try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+        } catch (error) {
+            console.log('Error with Geocode ', error);
+        }
+    };
+
+return (
+    <div className='search'>
+        <Combobox onSelect={ handleSelect }>
+            <ComboboxInput
+                value={ value }
+                onChange={ handleInput }
+                disabled={ !ready }
+                placeholder='Search a bar or something'
+            />
+            <ComboboxPopover>
+                <ComboboxList>
+                {status === 'OK' && data.map(({ id, description }) => (
+                    <ComboboxOption key={id} value={description} />
+                    ))}
+                </ComboboxList>
+            </ComboboxPopover>
+        </Combobox>
+    </div>
+    );
+}
     
   
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -11,54 +11,83 @@ import {
 } from '@reach/combobox';
 import '@reach/combobox/styles.css';
 
+// const panTo = useCallback(({ lat, lng }) => {
+//     mapRef.current.panTo({ lat, lng });
+//     mapRef.current.setZoom(14);
+//   }, []);
+
+function Locate({ panTo }) {
+    return (
+        <button
+            className='locate'
+            onClick={() => {
+                navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    panTo({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                    });
+                },
+                () => null
+                );
+            }}
+        >
+        <img src='https://www.kindpng.com/picc/m/11-115956_icon-google-maps-compass-north-hd-png-download.png' alt='compass' />
+        </button>
+    );
+}
+
 function Search({ panTo }) {
-    const {ready, 
+    const {
+        ready, 
         value, 
         suggestions: { status, data },
         setValue,
-        clearSuggestions, } = 
-        usePlacesAutoComplete({
+        clearSuggestions, } 
+        = usePlacesAutoComplete({
         requestOptions: {
-            location: {lat: () => 38.8960499, lng: () => -77.0648878},
+            location: { lat: () => 38.8960499, lng: () => -77.0648878 },
             radius: 150 * 1000, 
-        }
+        },
     });
 
     const handleInput = (e) => {
         setValue(e.target.value);
-      };
-    
-      const handleSelect = async (address) => {
+        };
+
+    const handleSelect = async (address) => {
         setValue(address, false);
         clearSuggestions();
-    
-        try {
-          const results = await getGeocode({ address });
-          const { lat, lng } = await getLatLng(results[0]);
-          panTo({ lat, lng });
-        } catch (error) {
-          console.log("Error with Geocode ", error);
-        }
-      };
 
-    return (
-        <div className='search'>
-            <Combobox onSelect={ handleSelect }>
-                <ComboboxInput 
-                value={value} 
+        try {
+            const results = await getGeocode({ address });
+            const { lat, lng } = await getLatLng(results[0]);
+            panTo({ lat, lng });
+        } catch (error) {
+            console.log('Error with Geocode ', error);
+        }
+    };
+
+return (
+    <div className='search'>
+        <Combobox onSelect={ handleSelect }>
+            <ComboboxInput
+                value={ value }
                 onChange={ handleInput }
-                disabled={!ready}
-                placeholder='enter an address'
-                />
-            
-                <ComboboxPopover> 
-                    {status === 'OK' && data.map(({id, description}) => (
-                        <ComboboxOption key={id} value={description} />
-                    )) }
-                </ComboboxPopover>
-            </Combobox>
-        </div>
+                disabled={ !ready }
+                placeholder='Search a bar or something'
+            />
+            <ComboboxPopover>
+                <ComboboxList>
+                {status === 'OK' && data.map(({ id, description }) => (
+                    <ComboboxOption key={id} value={description} />
+                    ))}
+                </ComboboxList>
+            </ComboboxPopover>
+        </Combobox>
+    </div>
     );
 }
+
 
 export default Search
